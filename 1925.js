ef4a3b2209d5ad24d86580e042b44cd3c0a4b3a1
Mehedi Hasan Ridoy <ridoy.starlord@gmail.com>
2023-01-05 16:54:39 +0600

    Solve ount Square Sum Triples

---
+++ 1925.js
@@ -0,0 +1,19 @@
+var countTriples = function (n) {
+  let arr = [];
+  let count = 0;
+  for (let i = 0; i < n; i++) {
+    arr[i] = i + 1;
+  }
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      for (let k = j + 1; k < n; k++) {
+        if (Math.pow(arr[i], 2) + Math.pow(arr[j], 2) === Math.pow(arr[k], 2)) {
+          count++;
+        }
+      }
+    }
+  }
+  return count * 2;
+};
+
+console.log(countTriples(10));
